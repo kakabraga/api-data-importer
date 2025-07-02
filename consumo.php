eff3e1df2cd52cd80bd70ba1c95f90532ed3af0e
kakabraga <167010103+kakabraga@users.noreply.github.com>
2025-07-02 14:22:14 -0300

    Finalização da inserção dos dados

--- consumo.php
+++ consumo.php
@@ -1,6 +1,6 @@
 <?php
 include 'Database.php';
-$url = "https://restcountries.com/v3.1/name/Brazil";
+$url = "https://restcountries.com/v3.1/all?fields=name,capital,currencies";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // retorna como string
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // ignora SSL (somente para testes)
@@ -8,21 +8,21 @@ $resposta = curl_exec($ch);
 curl_close($ch);
 
 $dados = json_decode($resposta, true);
-// echo "<pre>";
-// print_r($dados);
-// echo "</pre>";
-foreach ($dados as $d) {
-    $common   = $d['name']['common'];
-    $official = $d['name']['official'];
-    $language = $d['languages']['por'];
-    $moeda    = $d['currencies']['BRL']['name'];
-    $continente = $d['region'];
+echo "<pre>";
+print_r($dados);
+echo "</pre>";
+foreach ($dados as $key => $dado) {
+    $common   = $dado['name']['common'];
+    $official = $dado['name']['official'];
+    foreach($dado['currencies'] as $moeda) {
+        $moedas = $moeda['name'];
+    };
+    $capital = $dado['capital']['0'];
+    $stmt = $conn->prepare("INSERT INTO pais (nome_comum, nome_oficial, capital, moeda) values(:nome_comum, :nome_oficial, :capital, :moeda)");
+    $stmt->bindParam(':nome_comum', $common);
+    $stmt->bindParam(':nome_oficial', $official);
+    $stmt->bindParam(':capital', $capital);
+    $stmt->bindParam(':moeda', $moedas);
+    $stmt->execute();
 }
 
-$stmt = $conn->prepare("INSERT INTO pais (nome_comum, nome_oficial, lingua, moeda, continente) values(:nome_comum, :nome_oficial, :lingua, :moeda, :continente)");
-$stmt->bindParam(':nome_comum', $common);
-$stmt->bindParam(':nome_oficial', $official);
-$stmt->bindParam(':lingua', $language);
-$stmt->bindParam(':moeda', $moeda);
-$stmt->bindParam(':continente', $continente);
-$stmt->execute();
